c5fb03d649d6adfa785d8acd182327b143437f88
gaku3601 <pro.gaku@gmail.com>
2019-05-10 09:50:04 +0900

    watchで監視して開発できるように設定

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,21 @@
 module.exports = {
+    mode: 'production',
     entry: './src/index.ts',
     target: 'node',
     module: {
       rules: [
+        {
+          enforce: 'pre',
+          test: /\.(ts|tsx)$/,
+          use: [
+            {
+              loader: 'tslint-loader',
+              options: {
+                configFile: './tslint.json'
+              },
+            },
+          ],
+        },
         {
           test: /\.ts$/,
           use: ['ts-loader'],
