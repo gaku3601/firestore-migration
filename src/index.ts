f1c6c9cba7ae485c1f0ec31b5352ad736eae5379
gaku3601 <pro.gaku@gmail.com>
2019-05-13 11:20:58 +0900

    create時、時間とファイル名を付けれるように実装

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import * as commanders from 'commander';
 import * as fs from 'fs';
+import * as moment from 'moment';
 commanders
   .version('0.0.1', '-v, --version')
   .option('-t, --test', 'test option')
-  .option('-c, --create', 'create option')
+  .option('-c, --create <FileName>', 'create option')
   .parse(process.argv);
 const createFile = async () => {
   const tmp = await import('./templates/migration-file.tmp');
-  const str: string = tmp.default.replace(/\\n/g, '\n');
-  fs.writeFile('テストoutput2.txt', str, (err) => {
+
+  const date: string = moment().format("YYYYMMDDHHmmssSSS");
+  fs.writeFile(`${date}_${commanders.opts().create}.json`, tmp.default, (err) => {
     if (err) {
       console.log(err);
     }
