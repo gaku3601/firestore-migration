a3afdac6828653926946657985c1835affbda6af
gaku3601 <pro.gaku@gmail.com>
2019-05-14 07:48:20 +0900

    migration作成コマンドの関数をクラス化

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,15 @@
 import * as commanders from 'commander';
-import * as fs from 'fs';
-import * as moment from 'moment';
 import fileOperation from './fileOperation';
+import createMigration from './createMigration';
 commanders
   .version('0.0.1', '-v, --version')
   .option('-t, --test', 'test option')
   .option('-c, --create <FileName>', 'create option')
   .option('-m, --migrate <DirPath>', 'migration!')
   .parse(process.argv);
-const createFile = async () => {
-  const tmp = await import('./templates/migration-file.tmp');
-
-  const date: string = moment().format('YYYYMMDDHHmmssSSS');
-  fs.writeFile(`${date}_${commanders.opts().create}.json`, tmp.default, (err) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log('complete');
-  });
-};
 if (commanders.create) {
-  createFile();
+  const c = new createMigration()
+  c.createFile(commanders.opts().create);
 }
 if (commanders.migrate) {
   const f = new fileOperation(commanders.opts().migrate);
