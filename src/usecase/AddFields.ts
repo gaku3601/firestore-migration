e932d6f3e3dee09acfc2717e064f2a9522f074b7
gaku3601 <pro.gaku@gmail.com>
2019-05-23 09:26:42 +0900

    validation機能を実装

--- src/usecase/AddFields.ts
+++ src/usecase/AddFields.ts
@@ -5,6 +5,17 @@ export default class {
     constructor(collection: string, params: Param[]) {
         this.collection = collection;
         this.params = params;
+        this.validation(params);
+    }
+    private validation(params: Param[]) {
+        for (const param of params) {
+            if (!param.name) {
+                throw new Error('params[name]に値が設定されていません。');
+            }
+            if (!param.value) {
+                throw new Error('params[value]に値が設定されていません。');
+            }
+        }
     }
     get Collection(): string {
         return this.collection;
--- test/usecase/AddFields.ts
+++ test/usecase/AddFields.ts
@@ -2,9 +2,16 @@ import addFields from '@/usecase/AddFields';
 import { assert } from 'chai';
 
 describe('addFields class', () => {
-    it('collectionとparamsが設定されること', () => {
-        const a = new addFields('test', [{name: '', if: '', value: '', to: '', aggCollection: '', aggField: ''}]);
-        assert.deepEqual(a.Collection, 'test');
-        assert.deepEqual(a.Params, [{name: '', if: '', value: '', to: '', aggCollection: '', aggField: ''}]);
+    it('paramsに必要なnameに値がされていない場合、errorが例外として投げられること', () => {
+        assert.throw(() => {
+            // tslint:disable-next-line
+            new addFields('test', [{name: '', if: '', value: 'aaa', to: '', aggCollection: '', aggField: ''}]);
+        }, 'params[name]に値が設定されていません。');
+    });
+    it('paramsに必要なvalueに値が設定されていない場合、errorが例外として投げられること', () => {
+        assert.throw(() => {
+            // tslint:disable-next-line
+            new addFields('test', [{name: 'aaa', if: '', value: '', to: '', aggCollection: '', aggField: ''}]);
+        }, 'params[value]に値が設定されていません。');
     });
 });
